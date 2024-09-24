52edc7ec0bc13188d6a3f99692ac23534bd287aa
eawebmaster20 <eaweb20@gmail.com>
2024-09-24 17:55:16 +0000

    feat: create and populate login template and styles

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [MatButtonModule, MatIconModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+}
