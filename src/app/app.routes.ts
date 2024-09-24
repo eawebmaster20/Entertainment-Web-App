55754ed0c2d805336fd3d092e2faa34882aff279
eawebmaster20 <eaweb20@gmail.com>
2024-09-24 17:54:10 +0000

    feat: set up routes and router outlet for navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'signup', loadComponent: () =>import('./auth/signup/signup.component').then(m=>m.SignupComponent)},
+    {path: 'login', loadComponent: () =>import('./auth/login/login.component').then(m=>m.LoginComponent)},
+    {path: '', loadComponent: () =>import('./components/content/content.component').then(m=>m.ContentComponent)},
+    {path: '**', redirectTo: ''}
+];
