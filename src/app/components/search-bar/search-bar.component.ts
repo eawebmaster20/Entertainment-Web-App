9d81759b17f1bf10c239fea94d569450c543385f
eawebmaster20 <eaweb20@gmail.com>
2024-09-26 22:43:48 +0000

    core: create and implement a search bar component

---
+++ src/app/components/search-bar/search-bar.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { DataService } from '../../shared/services/data/data.service';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-search-bar',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './search-bar.component.html',
+  styleUrl: './search-bar.component.scss'
+})
+export class SearchBarComponent {
+ constructor(public dataService: DataService){}
+}
