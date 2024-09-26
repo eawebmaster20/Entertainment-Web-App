06b47478dd32389acb421d355b8b88fcc4eee832
eawebmaster20 <eaweb20@gmail.com>
2024-09-26 22:38:44 +0000

    add a store action dispatcher

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ApiService } from './shared/services/api/api.service';
+import { DataService } from './shared/services/data/data.service';
+import { Store } from '@ngrx/store';
+import { fetchMovies } from './shared/state/board.actions';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +12,14 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'EntertainmentWebApp';
+export class AppComponent implements OnInit {
+  constructor(private api:ApiService, private dataService:DataService, private store:Store){}
+  ngOnInit(): void {
+    this.store.dispatch(fetchMovies());
+    // this.store.dispatch(fetchDataAction())
+    // this.api.getMovies().subscribe({
+    //   next: (movies) => this.dataService.movies.next(movies),
+    //   error: (error) => console.error('Error fetching movies:', error)
+    // });
+  }
 }
