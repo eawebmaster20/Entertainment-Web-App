4ba57f280e42cd9af5ef3cf3f8e8f6d9885b4c15
eawebmaster20 <eaweb20@gmail.com>
2024-09-27 16:10:13 +0000

    fix: search result length show

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { jwtInterceptor } from './auth/interceptors/jwt/jwt.interceptor';
 import { provideState, provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
@@ -14,7 +14,8 @@ import { BoardEffects } from './shared/state/board.effects';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes), 
-    provideAnimationsAsync(),
+    // provideAnimationsAsync(),
+    provideAnimations(),
     provideHttpClient(withInterceptors([jwtInterceptor])),
     provideStore(),
     provideState({
