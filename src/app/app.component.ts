1e9af526c533e278b742de6a419baeb2dbfb3b7c
eawebmaster20 <eaweb20@gmail.com>
2024-09-29 01:40:53 +0000

    feat: inject notification toasters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,16 @@ import { RouterOutlet } from '@angular/router';
 import { ApiService } from './shared/services/api/api.service';
 import { DataService } from './shared/services/data/data.service';
 import { Store } from '@ngrx/store';
-import { fetchMovies } from './shared/state/board.actions';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ToastModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [MessageService]
 })
 export class AppComponent implements OnInit {
   constructor(private api:ApiService, private dataService:DataService, private store:Store){}
