ad1f635ff1357fd42ef821977c526bf52792391e
eawebmaster20 <eaweb20@gmail.com>
2024-09-30 09:47:10 +0000

    feat: create notification service for toast

---
+++ src/app/shared/services/notification/notification.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { MessageService } from 'primeng/api';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotificationService {
+  constructor(private messageService: MessageService) {}
+
+  showSuccess(summary: string, detail: string) {
+    this.messageService.add({severity: 'success', summary, detail});
+  }
+
+  showError(summary: string, detail: string) {
+    this.messageService.add({severity: 'error', summary, detail});
+  }
+
+  // Other toast types (info, warn, etc.) can be added similarly
+}
