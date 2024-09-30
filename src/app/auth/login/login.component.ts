5d62f469497694039ee72b2ee6f606daf09ab48d
eawebmaster20 <eaweb20@gmail.com>
2024-09-30 09:29:09 +0000

    fix: add loading spinner to login page

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {FormsModule} from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { ApiService } from '../../shared/services/api/api.service';
 import { ILogHttpLoginRes } from '../../shared/models/http.interface';
-import { MessageService } from 'primeng/api';
+import { MessageService, PrimeNGConfig } from 'primeng/api';
 import { ToastModule } from 'primeng/toast';
 import { JsonPipe } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
@@ -16,33 +16,40 @@ import { ButtonModule } from 'primeng/button';
   imports: [MatButtonModule, MatIconModule, FormsModule, RouterLink, ToastModule, JsonPipe,ButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
-  providers: [MessageService]
+  providers: [MessageService],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   userCredentials = {
     email: '',
     password: ''
   }
+  loading:boolean = false;
   constructor(
     private router:Router,
     private apiService: ApiService,
-    private messageService: MessageService
-  ) {
+    private messageService: MessageService,
+    private primengConfig: PrimeNGConfig,
+  ) {  }
+
+  ngOnInit(): void {
+    this.primengConfig.ripple = true;
   }
   login(){
+    this.loading = true;
     console.log(this.userCredentials);
     this.apiService.login(this.userCredentials)
       .subscribe({
         next: (user:ILogHttpLoginRes) => {
           console.log(user);
+          this.loading=false;
           if (user && user.token) {
-            this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Successfully logged in' });
             this.createUser(this.userCredentials.email, user.token)
-            this.show('Logged in successfully', 'success');
+            this.show('Logged in successfully', 'contrast');
             console.log('Logged in successfully', user);
           }
         },
         error: (error) => {
+          this.loading=false;
           console.error('Error logging in', error);
         }
       })
